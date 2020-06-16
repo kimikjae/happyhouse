cac52fdd8aa86fcd8c16931d3b8fa6b7d32c3a4a
kimikjae <micael0919@gmail.com>
2020-06-16 11:11:34 +0900

    aa

---
+++ src/main/java/com/ssafy/happyhouse/model/dao/InterestLocationDaoImpI.java
@@ -0,0 +1,29 @@
+package com.ssafy.happyhouse.model.dao;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.happyhouse.model.dto.InteractionDto;
+
+
+@Repository
+public class InterestLocationDaoImpI implements InterestlocationDao {
+	private final static String NS = "com.ssafy.happyhouse.model.dao.InterestlocationDao.";
+
+	@Autowired
+	private SqlSession sqlSession;
+	@Override
+	public void insertLocation(InteractionDto interDto) {
+		sqlSession.insert(NS+"insertLocation",interDto);
+	}
+	public List<InteractionDto> selectInterestionById(String memberid) throws IOException {
+		return sqlSession.selectList(NS+"selectLocation", memberid);
+	}
+
+}
+
